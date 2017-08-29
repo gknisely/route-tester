d99bb5d8ac6161939c4375107d21d6a389a9a49a
gknisely <greg@mapzen.com>
2017-08-29 13:28:32 -0400

    changed back to auto

--- include_exclude/js/valhalla.js
+++ include_exclude/js/valhalla.js
@@ -2,7 +2,7 @@ var app = angular.module('routing', []);
 var hash_params = L.Hash.parseHash(location.hash);
 var mode_mapping = {
   'foot'    : 'pedestrian',
-  'car'     : 'bus',
+  'car'     : 'auto',
   'bicycle' : 'bicycle',
   'transit' : 'multimodal',
   'truck'   : 'truck'  
@@ -821,7 +821,7 @@ app.controller('RouteController', function($scope, $rootScope, $sce, $http) {
       driveBtn.addEventListener('click', function(e) {
         if (!rr) return;
         getEnvToken();
-        var costing = 'bus';
+        var costing = 'auto';
         var calendarInput = document.getElementById("datepicker").value;
         if (calendarInput != "") {
           dateStr = datetimeUpdate(calendarInput);
